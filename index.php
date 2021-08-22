2697b249d40c754ac0d4476301d95d46cf4da3ff
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-08-23 01:05:01 +0800

    basic attendance php website

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-$title = 'Index';
-require_once 'includes/header.php'; ?>
-  <!-- 
+    $title = 'Index'; 
+
+    require_once 'includes/header.php'; 
+    require_once 'db/conn.php'; 
+
+    $results = $crud->getSpecialties();
+
+?>
+    <!-- 
         - First name
         - Last Name
         - Date of Birth (Use DatePicker)
@@ -9,10 +15,10 @@ require_once 'includes/header.php'; ?>
         - Email Address
         - Contact Number
      -->
+    <h1 class="text-center">Registration for IT Conference </h1>
 
-    <h1 class = "text-center">Registration for IT Conference</h1>
-    <form method="post" action="success.php">
-    <div class="form-group">
+    <form method="post" action="success.php" enctype="multipart/form-data">
+        <div class="form-group">
             <label for="firstname">First Name</label>
             <input required type="text" class="form-control" id="firstname" name="firstname">
         </div>
@@ -25,13 +31,12 @@ require_once 'includes/header.php'; ?>
             <input type="text" class="form-control" id="dob" name="dob">
         </div>
         <div class="form-group">
-        <label for="specialty">Area of Expertise</label>
-        <select class="form-select" id="specialty" name="specialty" aria-label="Default select example">
-  <option selected>Open this select menu</option>
-  <option>front end dev</option>
-  <option>back end dev</option>
-  <option>full stack dev</option>
-</select>
+            <label for="specialty">Area of Expertise</label>
+            <select class="form-control" id="specialty" name="specialty">
+                <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) {?>
+                   <option value="<?php echo $r['specialty_id'] ?>"><?php echo $r['name']; ?></option>
+                <?php }?>
+            </select>
         </div>
         <div class="form-group">
             <label for="email">Email address</label>
@@ -44,8 +49,14 @@ require_once 'includes/header.php'; ?>
             <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
         </div>
         <br/>
+        <div class="custom-file">
+            <input type="file" accept="image/*" class="custom-file-input" id="avatar" name="avatar" >
+            <label class="custom-file-label" for="avatar">Choose File</label>
+            <small id="avatar" class="form-text text-danger">File Upload is Optional</small>
+
+        </div>
         
-        <br>
+        
         <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
     </form>
 <br>
--- success.php
+++ success.php
@@ -1,17 +1,39 @@
 <?php
 $title = 'Success';
-require_once 'includes/header.php'; ?>
+require_once 'includes/header.php';
+require_once 'db/conn.php';
+
+if(isset($_POST['submit'])){
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $dob = $_POST['dob'];
+    $email = $_POST['email'];
+    $contact = $_POST['phone'];
+    $specialty = $_POST['specialty'];
+    $isSuccess = $crud -> insertAttendees($fname, $lname, $dob, $email,$contact,$specialty);
+    
+
+    if($isSuccess) {
+        echo '<h1 class="text-center text-success">You have been registered!</h1>';
+        include 'includes/successmessage.php';
+    }
+    else {
+        include 'includes/errormessage.php';
+    }
+}
+?>
 
 
-<h1 class="text-center text-success">You have been register!</h1>
 
-<div class="card" style="width: 18rem;">
+
+
+ <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">
                 <?php echo $_POST['firstname'] . ' ' . $_POST['lastname'];  ?>
             </h5>
             <h6 class="card-subtitle mb-2 text-muted">
-                <?php echo $_POST['specialty'];  ?>    
+                <?php echo $specialtyName['name'];  ?>    
             </h6>
             <p class="card-text">
                 Date Of Birth: <?php echo $_POST['dob'];  ?>
